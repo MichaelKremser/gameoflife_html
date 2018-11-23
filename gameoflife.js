36428199f5ad3294111b902663ede7b602646cf3
MichaelKremser <michael@mkcs.at>
2018-11-23 21:18:36 +0100

    Colored buttons, can work automatically

--- gameoflife.js
+++ gameoflife.js
@@ -8,12 +8,14 @@ const rectHeight = 10;
 const dead = 0;
 const alive = 1;
 // Colors
-const cellColors = [ 'red' /*dead*/, 'green' /*alive*/ ];
+const cellColors = [ 'orange' /*dead*/, 'yellow' /*alive*/ ];
 // Fields
 var field = new Array(rows); // current generation that is displayed
 var nextgen = new Array(rows); // next generation
 
 var cnt = 0;
+var iterations = 0;
+var auto_iter_timer = undefined;
 
 function getCtx() {
     var canvas = document.getElementById('canvas');
@@ -26,6 +28,19 @@ function body_onload() {
     drawField();
 }
 
+function showIterations() {
+    var iterations_element = document.getElementById('iterations');
+    iterations_element.innerText = iterations;
+}
+
+function activateTimer() {
+    auto_iter_timer = setInterval(iterateGenerationAndDraw, 500);
+}
+
+function deactivateTimer() {
+    clearInterval(auto_iter_timer);
+}
+
 function buildField() {
     for (var row = 0; row < rows; row++) {
         field[row] = new Array(columns);
@@ -36,25 +51,31 @@ function buildField() {
             cnt++;
         }
     }
+    iterations = 0;
+    showIterations();
     //alert('buildField() completed: ' + cnt + ' items');
     //alert('getNeighboursCount(1/1): ' + getNeighboursCount(1, 1, alive));
 }
 
-function initBlinker() {
+function initBlinker(drawFieldAfterInit) {
     field[1][2] = alive;
     field[1][3] = alive;
     field[1][4] = alive;
+    if (drawFieldAfterInit)
+        drawField();
 }
 
-function initArrow() {
+function initArrow(drawFieldAfterInit) {
     field[1][1] = alive;
     field[2][2] = alive;
     field[3][3] = alive;
     field[4][2] = alive;
     field[5][1] = alive;
+    if (drawFieldAfterInit)
+        drawField();
 }
 
-function initPulsator() {
+function initPentadecathlon(drawFieldAfterInit) {
     field[7][8] = alive;
     field[7][11] = alive;
     field[7][13] = alive;
@@ -73,6 +94,8 @@ function initPulsator() {
     field[9][14] = alive;
     field[9][16] = alive;
     field[9][19] = alive;
+    if (drawFieldAfterInit)
+        drawField();
 }
 
 function drawField() {
@@ -85,7 +108,11 @@ function drawField() {
     }
 }
 
-function iterateGeneration() {
+function iterateGenerationAndDraw() {
+    iterateGeneration(true);
+}
+
+function iterateGeneration(showFieldAfterInteration) {
     for (var row = 0; row < rows; row++) {
         for (var column = 0; column < columns; column++) {
             var livingNeighbours = getNeighboursCount(row, column, alive);
@@ -111,6 +138,10 @@ function iterateGeneration() {
             field[row][column] = nextgen[row][column];
         }
     }
+    iterations++;
+    showIterations();
+    if (showFieldAfterInteration)
+        drawField();
 }
 
 function getNeighboursCount(row, column, cellState) {
